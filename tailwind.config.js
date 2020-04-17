db30e03a8930293ac8de18446ef7f92f3f0e1952
David Daza <03dazal@gmail.com>
2020-04-17 16:36:45 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        eden: '#144f4b',
+        elephant: '#0e3b3a',
+        'mine-shaft': '#232323',
+        paper: '#fbf9f9',
+      },
+      fontFamily: {
+        'pt-sans-narrow': ['"PT Sans Narrow"', 'sans-serif'],
+        'source-serif': ['"Source Serif Pro"', 'serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
